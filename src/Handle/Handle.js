80c213df2bd477d4c4cae67ea7f73e3054eab2f7
Johnny Prescott <drizztdourden_@hotmail.com>
2021-02-15 23:33:35 -0500

    FirstRealCommit

---
+++ src/Handle/Handle.js
@@ -0,0 +1,12 @@
+import React, { useEffect } from 'react'
+import './Handle.css';
+
+import {BiDotsHorizontalRounded} from 'react-icons/bi';
+
+const Handle = () => {
+    return <div className="Handle">
+        <a  className="puller"><BiDotsHorizontalRounded /></a>
+    </div>
+};
+
+export default Handle;
