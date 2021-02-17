1a36677fec0ccf7b6fd37e90099d17b9c121e479
Johnny Prescott <drizztdourden_@hotmail.com>
2021-02-16 23:51:43 -0500

    Remove taskbard, added tray, slide in added

--- public/electron.js
+++ public/electron.js
@@ -1,5 +1,6 @@
 const electron = require("electron");
-const app = electron.app;
+const { app, Menu, Tray } = require("electron");
+
 const standardWindow = electron.BrowserWindow;
 const {BrowserWindow} = require("electron-acrylic-window");
 const path = require("path"); 
@@ -8,13 +9,15 @@ const isDev = require("electron-is-dev");
 let mainWindow;
 let handle;
 
+ipc = electron.ipcMain;
+
 function createMain() {
     vOptions = {
         theme: 'appearance-based',
         effect: 'acrylic',
         useCustomWindowRefreshMethod: true,
         maximumRefreshRate: 60,
-        disableOnBlur: true
+        disableOnBlur: false
      }
 
     mainWin = new BrowserWindow({ 
@@ -25,6 +28,8 @@ function createMain() {
         frame: false,
         transparent: true,
         alwaysOnTop: true,
+        resizable: false,
+        skipTaskbar: true,
         vibrancy: vOptions,
         webPreferences: { 
             webSecurity: false, 
@@ -43,11 +48,7 @@ function createMain() {
 };
 
 function createHandle() {
-    var screen = electron.screen;
-    var mainScreen = screen.getPrimaryDisplay()
-    const dimensions = mainScreen.size;
-    const screenWidth = dimensions.width;
-
+    screenWidth = getScreenWidth();
     const centeredX = (screenWidth / 2) - (100 / 2);
 
     handleWin = new standardWindow({ 
@@ -58,6 +59,8 @@ function createHandle() {
         frame: false,
         transparent: true,
         alwaysOnTop: true,
+        resizable: false,
+        skipTaskbar: true,
         webPreferences: { 
             webSecurity: false, 
             nodeIntegration: true,
@@ -84,5 +87,74 @@ app.on("ready", () => {
     })
 });
 
+let tray = null
+app.whenReady().then(() => {
+    tray = new Tray('public/logo32@2x.png');
+    const contextMenu = Menu.buildFromTemplate([
+        { label: 'Exit', type: 'radio' }
+      ]);
+    tray.setToolTip('Kanote')
+    tray.setContextMenu(contextMenu)
+});
+
 app.on("window-all-closed", () => { if (process.platform !== "darwin") { app.quit(); } });     
 app.on("activate", () => { if (mainWindow === null) { createWindow(); }}); 
+
+// --------------- POST READY STUFF --------------- //
+ipc.on('ToggleScroll', (event, args) => {
+    
+    screenWidth = getScreenWidth();
+    const [appWidth, appHeight] = mainWindow.getSize();
+    let [appX, appY] = mainWindow.getPosition()
+
+    let appToggled;
+    (appY < 0) ? appToggled = false : appToggled = true;
+    console.log(appY);
+    console.log(appHeight);
+    console.log("Received");
+    const centeredMain = (screenWidth / 2) - ((310 * 3) / 2);
+    const centeredHandle = (screenWidth / 2) - (100 / 2);
+
+    let intervalCount = 0;
+    let intervalMax;
+    let modifier = 20;
+
+    if (appToggled) {
+        modifier *= -1;
+        appY = 0; //Ensure starting position when moving in case anything happened that moved the normal position.
+    }
+    else appY = -appHeight;
+    
+    intervalMax = Math.round(appHeight / Math.abs(modifier));
+    console.log(intervalMax);
+    const iID = setInterval(() => {
+        if (intervalCount <= intervalMax) {
+            let y = appY + (intervalCount * modifier);            
+            mainWindow.setPosition(centeredMain, y);
+            handle.setPosition(centeredHandle, y + appHeight);
+
+        }
+        else{
+            let y = appY + (intervalMax * modifier);
+            mainWindow.setPosition(centeredMain, y);
+            handle.setPosition(centeredHandle, y + appHeight);            
+            clearInterval(iID);
+            console.log("Moved at " + y);
+        };
+        intervalCount++;
+    }, 5);
+});
+
+getScreenWidth = () => {
+    var screen = electron.screen;
+    var mainScreen = screen.getPrimaryDisplay()
+    const dimensions = mainScreen.size;
+    return dimensions.width;
+}
+
+getScreenHeight = () => {
+    var screen = electron.screen;
+    var mainScreen = screen.getPrimaryDisplay()
+    const dimensions = mainScreen.size;
+    return dimensions.height;
+}
--- src/Handle/Handle.js
+++ src/Handle/Handle.js
@@ -3,10 +3,19 @@ import './Handle.css';
 
 import {BiDotsHorizontalRounded} from 'react-icons/bi';
 
+const ipc = window.require('electron').ipcRenderer;
+
+
 const Handle = () => {
+
+    const toggleScroll = () => {
+        ipc.send('ToggleScroll');
+    };
+
     return <div className="Handle">
-        <a  className="puller"><BiDotsHorizontalRounded /></a>
+        <a onClick={toggleScroll} className="puller"><BiDotsHorizontalRounded /></a>
     </div>
 };
+
 
 export default Handle;
