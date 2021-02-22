64a6f721bd8c506dd65e5870d6e7b76dbde58eea
Johnny Prescott <drizztdourden_@hotmail.com>
2021-02-21 23:24:47 -0500

    Added click-through transparency. Working for now.

--- public/electron.js
+++ public/electron.js
@@ -20,8 +20,8 @@ function createMain() {
         disableOnBlur: false
      }
 
-    mainWin = new BrowserWindow({ 
-        y:0,
+    mainWin = new standardWindow({ 
+        y:36,
         x:0,
         width: 930, 
         height: 600,
@@ -30,7 +30,6 @@ function createMain() {
         alwaysOnTop: true,
         resizable: false,
         skipTaskbar: true,
-        vibrancy: vOptions,
         webPreferences: { 
             webSecurity: false, 
             nodeIntegration: true,
@@ -39,7 +38,7 @@ function createMain() {
         show: false
     }); 
 
-    mainWin.webContents.openDevTools();
+    //mainWin.webContents.openDevTools();
 
     mainWin.loadURL(isDev ? "http://localhost:3000" : `file://${path.join(__dirname, "../build/index.html")}`); 
     mainWin.on("closed", () => (mainWin = null));
@@ -47,19 +46,18 @@ function createMain() {
     return mainWin;
 };
 
-function createHandle() {
-    screenWidth = getScreenWidth();
-    const centeredX = (screenWidth / 2) - (100 / 2);
 
+
+function createHandle() {
     handleWin = new standardWindow({ 
-        y:600,
-        x:centeredX,
+        y:-35,
+        x:0,
         width: 100, 
-        height: 60,
+        height: 35,
         frame: false,
         transparent: true,
         alwaysOnTop: true,
-        resizable: false,
+        resizable: true,
         skipTaskbar: true,
         webPreferences: { 
             webSecurity: false, 
@@ -91,7 +89,7 @@ let tray = null
 app.whenReady().then(() => {
     tray = new Tray('public/logo32@2x.png');
     const contextMenu = Menu.buildFromTemplate([
-        { label: 'Exit', type: 'radio' }
+        { label: 'Exit'}
       ]);
     tray.setToolTip('Kanote')
     tray.setContextMenu(contextMenu)
@@ -100,72 +98,114 @@ app.whenReady().then(() => {
 app.on("window-all-closed", () => { if (process.platform !== "darwin") { app.quit(); } });     
 app.on("activate", () => { if (mainWindow === null) { createWindow(); }}); 
 
-ResizeCenterMain = (columnCount, rowMax) => {
-    console.log("Resizing");
+let lastColumnCount;
+let lastrowMax;
+
+ResizeCenterMain = (columnCount, rowMax, toggled) => {
+    let c, r;    
+    columnCount==='last' ? c = lastColumnCount: c = parseInt(columnCount);
+    rowMax==='last' ? r = lastrowMax: r = parseInt(rowMax);
+    
+    //Size is minimum for 3 columns and 3 rows.
+    c = Math.max(3, c);
+    r = Math.max(3, r);
+
+    lastColumnCount = c;
+    lastrowMax = r;
 
     screenWidth = getScreenWidth();
     const [appWidth, appHeight] = mainWindow.getSize();
     let [appX, appY] = mainWindow.getPosition()
 
-    const TargetWidth = (((300 + 10) * columnCount));
+    const colTitleH = 50;
+    const colAddH = 40;
+    const cardH = 120;
+    const cardMarginH = 10;
+    const appMarginH = 20;
+    const bottomBuffer = 50;
+    const TargetWidth = (((300 + 20) * c));
+    const TargetHeight = colTitleH + colAddH + (cardH * r) + (cardMarginH * (r + 1)) + (appMarginH * 2) + bottomBuffer;
     const centeredX = (screenWidth / 2) - (TargetWidth / 2);
-    mainWindow.setSize(TargetWidth, appHeight);
+
+    mainWindow.resizable = true;
+    mainWindow.setSize(TargetWidth, TargetHeight);
     mainWindow.setPosition(centeredX, appY);
+    mainWindow.resizable = false;
 
-    
+    let handleWidth = TargetWidth + 10;
+    let handleHeight = 0; 
 
+    toggled ? handleHeight = 45 + 10 : handleHeight = 35; // +10 compensate for unclickable reserved zone for resize
+    const handleCenteredX = (screenWidth / 2) - (handleWidth / 2);
+
+    console.log("toggle during resize: " + toggled);
+    console.log("New resize handle width: " + handleWidth);
+    console.log("New resize handle height:: " + handleHeight);
+
+    handle.resizable = true;
+    handle.setPosition(handleCenteredX, -5);
+    handle.setSize(handleWidth, handleHeight);
+    handle.resizable = false;
 };
 
-
 ipc.on('ResizeMainWindow', (event, args) => {
-    console.log("Received: ResizeMainWindow");
-    console.log(args[0]);
-    console.log(args[1]);
-    //ResizeCenterMain(args[0], args[1]);
+    ResizeCenterMain(args[0], args[1], args[2]);
 });
 
-// --------------- POST READY STUFF --------------- //
-ipc.on('ToggleScroll', (event, args) => {
-    
+
+const ToggleScroll = () => {
     screenWidth = getScreenWidth();
     const [appWidth, appHeight] = mainWindow.getSize();
     let [appX, appY] = mainWindow.getPosition()
     let [handleX, handleY] = handle.getPosition()
 
     let appToggled;
-    (appY < 0) ? appToggled = false : appToggled = true;
-    console.log(appY);
-    console.log(appHeight);
-    console.log("Received");
+    if(appY < 0) {
+        console.log("OFF BEFORE");
+        appToggled = false;
+    }
+    else{
+        console.log("ON BEFORE");
+        appToggled = true;
+    }
 
     let intervalCount = 0;
     let intervalMax;
     let modifier = 20;
 
     if (appToggled) {
+        mainWindow.alwaysOnTop = true;
         modifier *= -1;
         appY = 0; //Ensure starting position when moving in case anything happened that moved the normal position.
     }
-    else appY = -appHeight;
-    
-    intervalMax = Math.round(appHeight / Math.abs(modifier));
-    console.log(intervalMax);
+    else {
+        mainWindow.alwaysOnTop = true;
+        appY = -appHeight;
+    }
+
+    intervalMax = Math.round((appHeight + 35) / Math.abs(modifier));
     const iID = setInterval(() => {
         if (intervalCount <= intervalMax) {
             let y = appY + (intervalCount * modifier);            
             mainWindow.setPosition(appX, y);
-            handle.setPosition(handleX, y + appHeight);
         }
         else{
             let y = appY + (intervalMax * modifier);
-            mainWindow.setPosition(appX, y);
-            handle.setPosition(handleX, y + appHeight);            
+            mainWindow.setPosition(appX, y);          
             clearInterval(iID);
-            console.log("Moved at " + y);
         };
         intervalCount++;
     }, 5);
+    return appToggled;
+}
+
+ipc.handle('ToggleScroll', (event, args) => {
+    const toggleState =  ToggleScroll();
+    console.log("AFTER: " + toggleState);
+    return toggleState;
 });
+
+
 
 getScreenWidth = () => {
     var screen = electron.screen;
