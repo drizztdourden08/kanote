661f7a48dcf532f2f379d91c47ff96af27b1eebc
Johnny Prescott <drizztdourden_@hotmail.com>
2021-05-31 00:03:44 -0400

    commit card js

--- src/Main/components/Card.js
+++ src/Main/components/Card.js
@@ -14,96 +14,83 @@ const Card = (props) => {
         <div>
             <Draggable key={props.card.id} draggableId={props.card.id} index={props.index} >
                 {(provided) => (
-                    <div
-                        className="card"
-
+                    <div className="card"
                         {...provided.draggableProps}
                         ref={provided.innerRef}
                     >
-                        {
-                            <div className="card-wrapper">
-                                <div className="card-top">
+                        <div className="card-header">
+                            <div className="card-main">
+                                <div className="card-main_wrapper">
                                     {
-                                        props.card._imageSource ?
-                                            <div className="card-image">
-                                                <img alt="header-img" src={props.card._imageSource} />
+                                        props.card.priority !== null ?
+                                            <div className="card_priority" style={{ background: props.card.priority.color }}>
+                                                <span>{props.card.priority.title}</span>
                                             </div>
                                             : undefined
                                     }
-                                    <div className="card-main">
-                                        <div className="card-main_wrapper">
-                                            {
-                                                props.card.priority !== null ?
-                                                    <div className="card_priority" style={{ background: props.card.priority.color }}>
-                                                        <span>{props.card.priority.title}</span>
-                                                    </div>
-                                                    : undefined
-                                            }
-                                            <div {...provided.dragHandleProps} className="card_title">
-                                                <span>{props.card.title}</span>
-                                            </div>
-                                            <div className="card-editing" >
-                                                <button className="standard-button" onClick={() => props.functions.updateCard(props.functions.getParentId('COLUMN', props.card.columnId), props.card.columnId, props.card.id, [{ 'property': 'editing', 'newValue': !props.card.editing }])} ><BiDotsVerticalRounded /></button>
-                                            </div>
+                                    <div {...provided.dragHandleProps} className="card_title">
+                                        <span>{props.card.title}</span>
+                                    </div>
+                                    <div className="card-editing" >
+                                        <button className="standard-button" onClick={() => props.functions.updateCard(props.functions.getParentId('COLUMN', props.card.columnId), props.card.columnId, props.card.id, [{ 'property': 'editing', 'newValue': !props.card.editing }])} ><BiDotsVerticalRounded /></button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="card-wrapper">
+                            <div className="card-field">
+                                <MdTitle />
+                                <input type="text" value={props.card.title} onChange={event => props.functions.updateCard(props.functions.getParentId('COLUMN', props.card.columnId), props.card.columnId, props.card.id, [{ 'property': 'title', 'newValue': event.target.value }])} />
+                            </div>
+                            <div className="card-field">
+                                <BiImageAlt />
+                                <input type="text" value={props.card.image} onChange={event => props.functions.updateCard(props.functions.getParentId('COLUMN', props.card.columnId), props.card.columnId, props.card.id, [{ 'property': 'image', 'newValue': event.target.value }])} />
+                            </div>
+                            <div className="card-field">
+                                <BiImageAlt />
+                                <input type="text" name="priority" value={props.card.priority} onChange={event => props.functions.updateCard(props.functions.getParentId('COLUMN', props.card.columnId), props.card.columnId, props.card.id, [{ 'property': 'priority', 'newValue': event.target.value }])} />
+                                <datalist id="priority">
+                                    <option value="Boston" />
+                                    <option value="Cambridge" />
+                                </datalist>
+                            </div>
+                        </div>
+                        {props.card.content.length > 0 ?
+                            <div className="card_content">
+                                {props.card.content.array.map((element, index) => {
+                                    return (
+                                        <div className="content-element" key={index}>
+                                            {props.functions.returnContentElement(element, index)}
+                                            <hr />
                                         </div>
-                                    </div>
-                                </div>
-                                <div className="card-wrapper">
-                                    <div className="card-field">
-                                        <MdTitle />
-                                        <input type="text" value={props.card.title} onChange={event => props.functions.updateCard(props.functions.getParentId('COLUMN', props.card.columnId), props.card.columnId, props.card.id, [{ 'property': 'title', 'newValue': event.target.value }])} />
-                                    </div>
-                                    <div className="card-field">
-                                        <BiImageAlt />
-                                        <input type="text" value={props.card.image} onChange={event => props.functions.updateCard(props.functions.getParentId('COLUMN', props.card.columnId), props.card.columnId, props.card.id, [{ 'property': 'image', 'newValue': event.target.value }])} />
-                                    </div>
-                                    <div className="card-field">
-                                        <BiImageAlt />
-                                        <input type="text" name="priority" value={props.card.priority} onChange={event => props.functions.updateCard(props.functions.getParentId('COLUMN', props.card.columnId), props.card.columnId, props.card.id, [{ 'property': 'priority', 'newValue': event.target.value }])} />
-                                        <datalist id="priority">
-                                            <option value="Boston" />
-                                            <option value="Cambridge" />
-                                        </datalist>
-                                    </div>
-                                </div>
-                                {props.card.content.length > 0 ?
-                                    <div className="card_content">
-                                        {props.card.content.array.map((element, index) => {
-                                            return (
-                                                <div className="content-element" key={index}>
-                                                    {props.functions.returnContentElement(element, index)}
-                                                    <hr />
-                                                </div>
-                                            );
-                                        })
-                                        }
+                                    );
+                                })
+                                }
+                            </div>
+                            : undefined
+                        }
+
+                        <div className="card_footer">
+                            <div className="card_footer-left">
+                                {props.card.tags.length > 0 ?
+                                    <div className="card_tags">
+                                        {props.card.tags.array.map((tag, index) =>
+                                            <div key={index} className="tag">{tag}</div>)}
                                     </div>
                                     : undefined
                                 }
-
-                                <div className="card_footer">
-                                    <div className="card_footer-left">
-                                        {props.card.tags.length > 0 ?
-                                            <div className="card_tags">
-                                                {props.card.tags.array.map((tag, index) =>
-                                                    <div key={index} className="tag">{tag}</div>)}
-                                            </div>
-                                            : undefined
-                                        }
-                                        {props.card.assignees.length > 0 ?
-                                            <div className="card_footer_assignees">
-                                                {props.card.assignees.array.map((assignee, index) =>
-                                                    <div className="assignee-wrapper" key={index}>
-                                                        <div className="assignee" style={{ background: assignee.color }}>{assignee.initial}</div>
-                                                    </div>)}
-                                            </div>
-                                            : undefined
-                                        }
+                                {props.card.assignees.length > 0 ?
+                                    <div className="card_footer_assignees">
+                                        {props.card.assignees.array.map((assignee, index) =>
+                                            <div className="assignee-wrapper" key={index}>
+                                                <div className="assignee" style={{ background: assignee.color }}>{assignee.initial}</div>
+                                            </div>)}
                                     </div>
-                                    <div className="card_footer_notes-counter"><BiCommentDetail /><span>{props.card.comments.array.length}</span></div>
-                                </div>
+                                    : undefined
+                                }
                             </div>
-                        }
+                            <div className="card_footer_notes-counter"><BiCommentDetail /><span>{props.card.comments.array.length}</span></div>
+                        </div>
                     </div>
                 )}
             </Draggable>
